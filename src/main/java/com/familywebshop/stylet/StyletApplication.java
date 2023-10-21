37f9e0786d81463edeb8b07421f02d83e5e81bfb
Hernádi Mihály <55395934+HernadiMihaly@users.noreply.github.com>
2023-10-21 21:49:55 +0200

    feat(src): initial project setup
    - Initial project setup with required dependencies and directory hierarchy.

---
+++ src/main/java/com/familywebshop/stylet/StyletApplication.java
@@ -0,0 +1,13 @@
+package com.familywebshop.stylet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StyletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/familywebshop/stylet/StyletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familywebshop.stylet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StyletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
